ea73aa86304cda9e2028f0530db5319b75d2c69b
nazirul <nazirul.me01c@gmail.com>
2024-03-02 01:53:36 +0600

    all done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cardContainer = document.getElementById("card-container");
 const searchInput = document.getElementById("search-input");
 const showAllBtn = document.getElementById("showall-btn");
+const modalContainer = document.getElementById("my_modal_5");
 
 const getApiResponse = async (brand) => {
   const responseText = await fetch(
@@ -23,7 +24,9 @@ const showCard = (data) => {
   data.forEach((phone) => {
     const cardDiv = document.createElement("div");
     cardDiv.className = "card bg-base-100 shadow-xl";
+    let phone_slug = phone.slug;
 
+    // console.log(typeof phone_slug);
     cardDiv.innerHTML = `
     <figure class="px-10 pt-10">
       <img
@@ -42,7 +45,7 @@ const showCard = (data) => {
       </p>
       <p class="price text-[#403F3F] font-bold text-2xl">$999</p>
       <div class="card-actions">
-        <button class="btn hover:text-black bg-[#0D6EFD] text-white" >
+        <button onclick='handleShowDetails("${phone_slug}")' class="btn hover:text-black bg-[#0D6EFD] text-white" >
           Show Details
         </button>
       </div>
@@ -61,4 +64,76 @@ const handleSearch = () => {
   getApiResponse(searchInputValue);
 };
 
-const handleShowAll = () => {};
+const handleShowAll = () => {
+  console.log("handle show is not shown in video");
+};
+
+const handleShowDetails = async (id) => {
+  const responseText = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const responseData = await responseText.json();
+  const phoneDetails = responseData.data;
+
+  modalContainer.innerHTML = `<div class="modal-box">
+  <div class="flex justify-center">
+    <img src="${phoneDetails.image}" alt="" />
+  </div>
+  <h3 class="font-bold text-xl">${phoneDetails.name}</h3>
+  <p class="py-4">
+    It is a long established fact that a reader will be distracted by
+    the readable content of a page when looking at its layout.
+  </p>
+
+  <div class="phone-info">
+    <p>
+      <span class="text-lg font-bold">Storage: </span> ${phoneDetails.mainFeatures.storage}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Display Size :</span> : ${phoneDetails.mainFeatures.displaySize}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Chipset :</span> : ${phoneDetails.mainFeatures.chipSet}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Memory :</span> : ${phoneDetails.mainFeatures.memory}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Slug :</span> :
+      ${phoneDetails.slug}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Release date :</span> :  ${phoneDetails.releaseDate}
+    </p>
+    <p><span class="text-lg font-bold">Brand : </span>  ${phoneDetails.brand}</p>
+    <p>
+      <span class="text-lg font-bold">GPS :</span>  ${phoneDetails.others.GPS}
+    </p>
+    <p>
+      <span class="text-lg font-bold">WLAN :</span>  ${phoneDetails.others.WLAN}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Bluetooth :</span>  ${phoneDetails.others.Bluetooth}
+    </p>
+    <p>
+      <span class="text-lg font-bold">NFC :</span>  ${phoneDetails.others.NFC}
+    </p>
+    <p>
+      <span class="text-lg font-bold">Radio :</span>  ${phoneDetails.others.Radio}
+    </p>
+    <p>
+      <span class="text-lg font-bold">USB :</span>  ${phoneDetails.others.USB}
+    </p>
+  </div>
+  <div class="modal-action">
+    <form method="dialog">
+   
+      <button class="btn bg-[#DC3545] text-white hover:text-black">
+        Close
+      </button>
+    </form>
+  </div>
+</div>`;
+  console.log(phoneDetails);
+  my_modal_5.showModal();
+};
